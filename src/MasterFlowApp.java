70ea6b6de272ed5bc6e4dcbbe49196da259593e7
jblaird1 <darrellblair@northwesternmutual.com>
2018-10-18 09:53:10 -0500

    Initial commit

---
+++ src/MasterFlowApp.java
@@ -0,0 +1,9 @@
+
+public class MasterFlowApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the initial release of MasterFlowApp.");
+	}
+
+}
