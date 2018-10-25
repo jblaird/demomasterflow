48aeef2345f5977cec77d4bc3f974d50d897ddec
jblaird1 <darrellblair@northwesternmutual.com>
2018-10-25 15:10:47 -0500

    Version 0.0.1-2nd Local Commit

--- src/MasterFlowApp.java
+++ src/MasterFlowApp.java
@@ -3,7 +3,7 @@ public class MasterFlowApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Version 0.0.1\nThis is the first local commit of MasterFlowApp changes.");
+		System.out.println("Version 0.0.1\nThis is the second local commit of MasterFlowApp changes.");
 	}
 
 }
